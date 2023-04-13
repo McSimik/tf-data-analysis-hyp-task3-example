6873e683269dbcb51b12d4a3c071d91c46bb56ce
McSimik <80466309+McSimik@users.noreply.github.com>
2023-04-13 21:59:27 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,12 @@
 import pandas as pd
 import numpy as np
+import statsmodels.stats.weightstats
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 964993301 # Ваш chat ID, не меняйте название переменной
 
 def solution(...) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    return 1-statsmodels.stats.weightstats.ztest(x,y)[1]<0.02
